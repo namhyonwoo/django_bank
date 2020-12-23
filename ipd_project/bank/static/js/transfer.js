a412fbdb70a2a8aa81acf36ab1c27600924c2fb0
cuya123 <cuya123@hotmail.com>
2020-12-23 21:51:20 +0900

    계좌이체 기능추가

---
+++ ipd_project/bank/static/js/transfer.js
@@ -0,0 +1,227 @@
+// transfer.js
+const transfer = (function(){
+
+	const exports = {};
+    const element = {};
+    const data = {
+        sender_account :'',
+        receiver_account :'',
+        sended_label :'',
+        received_label :'',
+        remittance : 0,
+        fee : 0,
+    };
+    
+	const setElements = function(){
+		element.csrftoken = $('input[name="csrfmiddlewaretoken"]');
+        
+        element.form = document.querySelector('#account_form');
+		element.inputSenderAccount = $('#sender_account');
+		element.inputReceiverAccount = $('#receiver_account');
+		element.inputSendedLabel = $('#sended_label');
+		element.inputReceivedLabel = $('#received_label');
+		element.inputRemittance = $('#remittance');
+        element.inputFee = $('#fee');
+        
+		element.viewSenderAccount = $('#view_sender_account');
+		element.viewReceiverAccount = $('#view_receiver_account');
+		element.viewSendedLabel = $('#view_sended_label');
+		element.viewReceivedLabel = $('#view_received_label');
+		element.viewRemittance = $('#view_remittance');
+        element.viewFee = $('#view_fee');
+
+	}
+	const addEvent = function(){
+        formBinder(element.form, beforeSave);
+
+        element.inputSenderAccount.change(function() {
+            getAmount($(this).val());
+        });
+
+        $('#exampleModal').on('shown.bs.modal', function () {
+            console.log('modal')
+        })
+    }
+
+	const ajaxSetup = function (){
+		$.ajaxSetup({
+			beforeSend:function(xhr, settings){
+                // show gif here, eg:
+                xhr.setRequestHeader("X-CSRFToken", element.csrftoken.val());
+				$("#wait").css("display", "block");
+			},
+			complete:function(){
+				// hide gif here, eg:
+				$("#wait").css("display", "none");
+			}
+		});
+	}
+
+	const formBinder = function (form, callback) {
+		if(!form) return;
+		form.addEventListener('submit', function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			if (form.checkValidity() === false) {
+				form.classList.add('was-validated');
+				form.querySelectorAll(':invalid')[0].focus();
+			}
+			else{
+				callback();
+			}
+		});
+    }
+    
+    // 계좌이체 데이터 저장
+	const makeData = function () {
+        data.sender_account = element.inputSenderAccount.val();
+        data.receiver_account = element.inputReceiverAccount.val();
+        data.sended_label = element.inputSendedLabel.val();
+        data.received_label = element.inputReceivedLabel.val();
+        data.remittance = element.inputRemittance.val();
+    }
+
+    // 계좌이체 데이터 세팅후 컴펌창 띄우기
+	const beforeSave = function () {
+        //수수료 계산
+        calcFee(function(){
+            //전역 data 세팅
+            makeData(); 
+            // 모달필드에 뷰값 채움
+            setModalView();
+            //컨펌창 모달 띄우기
+            $('#exampleModal').modal('show');
+        });
+    }
+
+	const setModalView = function () {
+        element.viewSenderAccount.text(data.sender_account);
+		element.viewReceiverAccount.text(data.receiver_account);
+		element.viewSendedLabel.text(data.sended_label);
+		element.viewReceivedLabel.text(data.received_label);
+		element.viewRemittance.text(data.remittance);
+        element.viewFee.text(data.fee);
+    }
+
+    // 계좌이체 실행
+	const save = function () {
+
+        console.log(data)
+
+        //요청
+        $.ajax({
+            method:'POST',
+            url: '/bank/transfer/',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            contentType:'application/json; charset=utf-8',
+        }).done(function(response) {
+            console.log(response);
+          
+        }).fail(function (error) {
+            console.log(JSON.stringify(error));
+        });
+    }
+
+    // 유효한 계좌인지 검사
+    const validateAccount = function () {
+        //상대계좌
+        receive_account = element.inputReceiverAccount.val();
+        if(!receive_account){
+            console.log("계좌번호입력");
+            return;
+        }
+        
+        const data={
+            account_id: receive_account
+        };
+        //요청
+        $.ajax({
+            method:'POST',
+			url: '/bank/validateAccount/',
+			data: JSON.stringify(data),
+			contentType:'application/json; charset=utf-8',
+			dataType: 'json',
+		}).done(function(response) {
+            console.log(response);
+            if(response.isValidAccount == true){
+                $('.receiver-account-info').html("<p>"+"올바른 계좌입니다. 소유자: "+response.owner_name+"</p>")
+            }else{
+                $('.receiver-account-info').html("<p>"+"없는 계좌입니다. </p>")
+            }
+		}).fail(function (error) {
+			console.log(JSON.stringify(error));
+		});
+    }
+    // 계좌에 얼마들었는지 조회
+    const getAmount = function(sender_account){
+          //상대계좌
+        //   sender_account = element.inputSenderAccount.val();
+          console.log('sender_account: '+sender_account);
+          if(!sender_account){
+              console.log("내계좌를 선택하세요");
+              return;
+          }
+          const account_id = sender_account
+          //요청
+          $.ajax({
+              method:'GET',
+              url: '/bank/amount/'+account_id,
+              dataType: 'json',
+          }).done(function(response) {
+              console.log(response);
+      
+          }).fail(function (error) {
+              console.log(JSON.stringify(error));
+          });
+    }
+
+    // 수수료 계산
+    const calcFee = function(callback){
+        
+        //내 계좌와 상대계좌를
+          const param_data={
+            sender_account : element.inputSenderAccount.val(),
+            receiver_account : element.inputReceiverAccount.val(),
+          };
+          $.ajax({
+              method:'GET',
+              url: '/bank/fee/',
+              dataType: 'json',
+              data: param_data
+          }).done(function(response) {
+            console.log(response)
+            fee = response.fee
+            data.fee=fee;
+            element.viewFee.html(fee);
+            callback();
+          }).fail(function (error) {
+             console.log('수수료 계산 실패',error);
+          });
+    }
+
+
+    /*exports*/
+	exports.init = function(){
+		setElements();
+		addEvent();
+		ajaxSetup();
+	};
+	exports.validateAccount = function(){
+		validateAccount();
+	};
+	exports.getAmount = function(){
+		getAmount();
+	};
+	exports.beforeSave = function(){
+		beforeSave();
+	};
+	exports.save = function(){
+		save();
+	};
+
+
+	return exports;
+})();
+
+transfer.init();
---
+++ ipd_project/bank/views.py
@@ -0,0 +1,222 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from http import HTTPStatus
+from django.urls import reverse
+from django.shortcuts import render
+from django.contrib import auth
+from .models import User, Account, AccountTransferReport
+from django.core import serializers
+import json
+from django.utils import timezone
+
+def index(request):
+
+    return render(request, 'index.html')
+
+
+class Example:
+    queue =[]# Access through class
+
+instance = Example()
+
+# 계좌이체 실행
+def transfer(request):
+    
+    # logined user
+    user = request.user
+
+    if request.method == 'POST':
+
+        queue = instance.queue 
+
+        print("queue is")
+        print(queue)
+
+        response = {
+            'is_succeful'  : False,
+            'error'  : ''
+        }
+        received_json_data = json.loads(request.body.decode("utf-8"))
+        sender_account = received_json_data['sender_account']
+        receiver_account = received_json_data['receiver_account']
+        sended_label = received_json_data['sended_label']
+        received_label = received_json_data['received_label']
+        remittance = received_json_data['remittance']
+        fee = received_json_data['fee']
+        # 파라미터가 비었다면
+        if sender_account is None or receiver_account is None or sended_label is None or received_label is None or remittance is None or fee is None:
+            response['error'] = 'param error'
+            return HttpResponse(json.dumps(response), 'application/javascript; charset=utf8')
+
+        ready_amount = int(remittance)+int(fee)
+        #  내 계좌에서 금액 남았는지 다시한번 조회
+        my_account = Account.objects.get(pk=sender_account)
+
+        my = None
+        for item in queue:
+            if item['id'] == sender_account:
+                my = item
+
+        print('my is')
+        print(my)
+
+        if my is not None:
+            if my['expectTotalAmount']+ready_amount > my_account.amount:
+                response['error'] = '남은금액 없음'
+                return HttpResponse(json.dumps(response), 'application/javascript; charset=utf8')
+            else:
+                print('있고 더했다')
+                my['expectTotalAmount'] += ready_amount
+        else:
+            print('my없음')
+            my = {
+                'id' : sender_account,
+                'expectTotalAmount' : ready_amount
+            }
+            queue.append(my)
+            # for item in queue:
+            #     if item['id'] == my['id']:
+            #         my = item
+
+        try:
+            # test code
+            # 실패
+            validate_transfer()
+
+            AccountTransferReport.objects.create(sender_account=Account.objects.get(pk=sender_account), receiver_account=Account.objects.get(pk=receiver_account), sended_label=sended_label, received_label=received_label, remittance=remittance, fee=fee)
+            
+            # refresh form db
+            my_account.refresh_from_db()
+            print('my_account amount is')
+            print(my_account.amount)
+            print('ready_amount is')
+            print(ready_amount)
+            
+            my_account.amount = my_account.amount-ready_amount
+            my_account.save()
+            my['expectTotalAmount'] -= ready_amount
+            print('반영됨=========')
+            
+            print('my: '+str(my))
+            print('queue: '+str(queue))
+
+            response['is_succeful'] = True
+
+        except Exception as e:
+            print('예외가 발생했습니다.', e)
+            my['expectTotalAmount'] -= ready_amount
+            response['error'] = e
+
+
+        return HttpResponse(json.dumps(response), 'application/javascript; charset=utf8')
+    
+    # get요청이면
+    else:    
+        account_list = Account.objects.filter(user=user)
+        context = {
+            'account_list' : account_list
+        }
+        return render(request, 'bank/transfer.html', context)
+
+
+
+from time import sleep
+from random import uniform as uniform_random
+def validate_transfer():
+    """
+    Internal Product Developer 기술 과제 2
+    이 함수는 본래 이체 결과를 검증하기 위한 함수입니다.
+    7 - 30초의 지연을 발생시킬 뿐이지만, 편의를 위해 본래의 역할을 한다고 가정해주세요.
+    """
+    sleep(uniform_random(7, 30))
+    return True
+
+
+
+# 유효한 계좌인지 조회
+def validateAccount(request):
+    received_json_data = json.loads(request.body.decode("utf-8"))
+    reponse = {
+        'isValidAccount' : False,
+        'owner_name'  : ''
+    }
+    account_id = received_json_data['account_id']
+    try:
+        account = Account.objects.get(pk=account_id)
+        reponse['isValidAccount'] = True
+        reponse['owner_name'] =  account.user.last_name + account.user.first_name
+
+    except Account.DoesNotExist:
+        pass
+    finally:
+        return HttpResponse(json.dumps(reponse),
+        content_type = 'application/javascript; charset=utf8')
+
+# 계좌에서 보유한 금액 조회
+def getAmount(request, account_id):
+    # 계좌번호
+    content = {
+        'amount' : 0,
+    }
+    # 있으면 반환
+    response = HttpResponse('', 'application/javascript; charset=utf8')
+    try:
+        account = Account.objects.get(pk=account_id)
+        content['amount'] =  account.amount
+        response.content = json.dumps(content)
+
+        return response
+
+    except Account.DoesNotExist:
+
+        response.status_code = HTTPStatus.NO_CONTENT
+        return response
+
+# 수수료 조회
+def getFee(request):
+    # 계좌번호
+    sender_account = request.GET['sender_account']
+    receiver_account = request.GET['receiver_account']
+    
+    content = {
+        'fee' : 500,
+    }
+    response = HttpResponse(content, 'application/javascript; charset=utf8')
+    # 세션이 없다면 종료
+    if request.user is None:
+        response.status_code = HTTPStatus.FORBIDDEN
+        return response
+
+    # 하루 무료건수 조회: fee_free_of_day
+    user = User.objects.get(pk=request.user.id)
+    fee_free_of_day = user.fee_free_of_day
+        
+    count = 0 # 무료건수확인변수
+    # 당월 이체건수 조회 후 (내계좌인데 타행계좌 또는 타계좌) n회 이하면 무료
+    today = timezone.now()
+    reports = AccountTransferReport.objects.filter(reg_date__year=today.year, reg_date__month=today.month, sender_account=sender_account).select_related('sender_account','receiver_account').all()
+
+    for report in reports:
+        if report.sender_account.user == report.receiver_account.user:
+            # 보낸 유저 아이디와 받은 유저 아이디 같으면서 통장종류가 같은 것을 제외
+            if report.sender_account.account_type == report.receiver_account.account_type:
+                print(report.receiver_account)
+                continue
+        count+=1
+
+    # print(f'count: {count}' )
+    # 수수료 한도 넘었나
+    if count >= fee_free_of_day:
+        receive_account = Account.objects.get(pk=receiver_account)
+        sender_account = Account.objects.get(pk=sender_account)
+        receive_user = receive_account.user
+        # 타 계좌일경우 :500원
+        if receive_user.id != user.id:
+            pass
+        # 내계좌인데 같은 종류:0원
+        elif receive_account.account_type == sender_account.account_type:
+            content['fee'] = 0
+    else:
+        content['fee'] = 0
+    # 응답 보냄
+    response.content = json.dumps(content)
+    return response
